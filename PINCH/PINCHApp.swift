76e697eee75b893454d713ef802caafd08413cfd
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-28 13:19:14 +0300

    Initial Commit

---
+++ PINCH/PINCHApp.swift
@@ -0,0 +1,17 @@
+//
+//  PINCHApp.swift
+//  PINCH
+//
+//  Created by Tacettin Küstü on 28.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PINCHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
