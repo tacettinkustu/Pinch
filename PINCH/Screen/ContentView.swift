5e4ae9be7dda79e6339f6d69fd1c5581daeaed1f
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-28 16:09:54 +0300

    Add navigation and image into contentView

--- PINCH/Screen/ContentView.swift
+++ PINCH/Screen/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y:2)
+            } //: ZStack
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+        } //: Navigation
+        .navigationViewStyle(.stack)
     }
 }
 
