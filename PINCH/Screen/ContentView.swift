8b02e6efc99e13ac79a97ee3b0c15c185017ec75
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-28 19:36:37 +0300

    Create a Drawer User Interface and implement it into main view

--- PINCH/Screen/ContentView.swift
+++ PINCH/Screen/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = CGSize(width: 0, height: 0)
+    @State private var isDrawerOpen: Bool = false
+    
+    @State private var pageIndex: Int = 1
     
     func resetImageState() {
         return withAnimation(.spring()) {
@@ -137,6 +140,31 @@ struct ContentView: View {
                 }
                     .padding(.bottom, 30)
                 , alignment: .bottom
+            )
+            .overlay(
+              HStack(spacing: 12) {
+                Image(systemName: isDrawerOpen ? "chevron.compact.right" :  "chevron.compact.left")
+                  .resizable()
+                  .scaledToFit()
+                  .frame(height: 40)
+                  .padding(8)
+                  .foregroundStyle(.secondary)
+                  .onTapGesture(perform: {
+                    withAnimation(.easeOut) {
+                      isDrawerOpen.toggle()
+                    }
+                  })
+                
+                Spacer()
+              } //: Drawer
+                .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                .background(.ultraThinMaterial)
+                .cornerRadius(12)
+                .opacity(isAnimating ? 1 : 0)
+                .frame(width: 260)
+                .padding(.top, UIScreen.main.bounds.height / 12)
+                .offset(x: isDrawerOpen ? 20 : 215)
+              , alignment: .topTrailing
             )
         } //: Navigation
         .navigationViewStyle(.stack)
