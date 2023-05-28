56d2d8b8e4189d48e59226e0f582adf06ecb7003
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-28 16:44:05 +0300

    Improve drag gesture and create a new reusable func

--- PINCH/Screen/ContentView.swift
+++ PINCH/Screen/ContentView.swift
@@ -13,6 +13,13 @@ struct ContentView: View {
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = CGSize(width: 0, height: 0)
     
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -31,9 +38,7 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
                     .gesture(
@@ -46,10 +51,7 @@ struct ContentView: View {
                             
                             .onEnded({ _ in
                                 if imageScale <= 1 {
-                                    withAnimation(.spring()) {
-                                        imageScale = 1
-                                        imageOffset = .zero
-                                    }
+                                    resetImageState()
                                 }
                             })
                     )
