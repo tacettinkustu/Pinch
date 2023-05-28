1a976543004016a90ce2dff0de36161e4dd839ec
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-28 16:39:36 +0300

    Implement drag gesture feature into contentView

--- PINCH/Screen/ContentView.swift
+++ PINCH/Screen/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = CGSize(width: 0, height: 0)
     
     var body: some View {
         NavigationView {
@@ -22,6 +23,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y:2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
@@ -34,6 +36,23 @@ struct ContentView: View {
                             }
                         }
                     })
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    withAnimation(.spring()) {
+                                        imageScale = 1
+                                        imageOffset = .zero
+                                    }
+                                }
+                            })
+                    )
             } //: ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
